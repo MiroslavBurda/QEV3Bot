84cfe2f59ce5c309a15823793551892b4820c792
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2022-08-28 14:49:51 +0200

    add promenne1.c

---
+++ promenne1.c
@@ -0,0 +1,13 @@
+
+
+/*	Program "Promìnné 1" pro simulátor QEV3BotSim
+
+*/
+
+task main()
+{
+	int pocet=0;  // nová promìnná typu int s pøiøazenou poèáteèní hodnotou 
+	pocet = pocet + 1; 
+	displayTextLine(3, "Poèet: %i", pocet);
+	delay(10000);	// èekání v milisekundách
+}
